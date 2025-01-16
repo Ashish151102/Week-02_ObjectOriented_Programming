2964da46e26473ab69fc28b3a0b05d966c80851a
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-16 23:04:24 +0530

    Added BookDetails.java: Handles book details functionality

---
+++ Level1/BookDetails.java
@@ -0,0 +1,25 @@
+
+class Book {
+    String title;
+    String author;
+    double price;
+
+    // Constructor to initialize book details
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 399.99);
+        book.displayDetails();
+    }
+}
