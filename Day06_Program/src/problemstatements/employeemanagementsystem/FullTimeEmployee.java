c7c628f578ab98a51dabad47b93a2f6ddd35f242
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-26 18:11:23 +0530

    Added Employee Management System with abstract class, subclasses, and interface implementation for managing employees and departments.

---
+++ Day06_Program/src/problemstatements/employeemanagementsystem/FullTimeEmployee.java
@@ -0,0 +1,18 @@
+package problemstatements.employeemanagementsystem;
+
+public class FullTimeEmployee extends Employee {
+    private double bonus;
+
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary, double bonus) {
+        super(employeeId, name, baseSalary);
+        this.bonus = bonus;
+    }
+
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() + bonus;
+    }
+
+}
---
+++ Day06_Program/src/problemstatements/employeemanagementsystem/HRDepartment.java
@@ -0,0 +1,18 @@
+package problemstatements.employeemanagementsystem;
+
+ class HRDepartment implements Department {
+     private String department;
+
+
+     @Override
+     public void assignDepartment(String department) {
+         this.department = department;
+     }
+
+
+     @Override
+     public void getDepartmentDetails() {
+         System.out.println("Department: " + department);
+     }
+
+ }
